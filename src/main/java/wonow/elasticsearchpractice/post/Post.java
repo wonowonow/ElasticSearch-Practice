7c586325f8444ea06658a93e02741d6316ecb8ce
wonowonow <wonow_@naver.com>
2024-03-08 22:59:41 +0900

    feat: Create Post Entity

---
+++ src/main/java/wonow/elasticsearchpractice/post/Post.java
@@ -0,0 +1,23 @@
+package wonow.elasticsearchpractice.post;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+}
